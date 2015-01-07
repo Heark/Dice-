b204ea02c41a5e1f1fa3db3adbcc06cc8cd55653
Heark <hearkofsorcerey@gmail.com>
2015-01-07 13:32:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-function start() {
-  
+var Speed = 5.5
+var Delta = 10.5
+var Players = function(Name, X, Y){
+  this.name = name;
+  this.X = X;
+  this.Y = Y;
+}
+var Player = new Players(playerName, 0, 0)
+
+function drawPlayer() {
+  var canvas = document.getElementById('canvas');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+
+    ctx.fillRect(Player.X,Player.Y,100,100);
+  }
+}
+
+function movePlayerRight() {
+  Player.X = Player.X * Speed + Delta
+  drawPlayer()
 }
